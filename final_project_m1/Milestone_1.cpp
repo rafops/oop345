7d4c8e8ba01918748ebc1d2a7a9c17a0b2580c30
Rafael Moraes <contact@moraesrafael.com>
2015-11-26 21:24:06 -0500

    reviewed Milestone_1.cpp

--- final_project_m1/Milestone_1.cpp
+++ final_project_m1/Milestone_1.cpp
@@ -11,86 +11,86 @@
 #include "Utilities.h"
 
 int main(int argc, char** argv) {
-    // process command line arguments
-    if (argc != 3) {
-        std::cerr << "*** invalid number of arguments ***\n"
-            << "Usage: " << argv[0] << " tasks_file field_delimiter\n";
-        return 1;
-    }
-    std::ifstream taskFile(argv[1]);
-    Task::setDelimiter(argv[2][0]);
-    if (!taskFile) {
-        std::cerr << "*** Cannot open tasks_file named " << argv[1] << " ***\n";
-        return 2;
-    }
-    Utilities::setDelimiter(argv[2][0]);
-    // end of command line processing
+  // process command line arguments
+  if (argc != 3) {
+    std::cerr << "*** invalid number of arguments ***\n"
+      << "Usage: " << argv[0] << " tasks_file field_delimiter\n";
+    return 1;
+  }
+  std::ifstream taskFile(argv[1]);
+  Task::setDelimiter(argv[2][0]);
+  if (!taskFile) {
+    std::cerr << "*** Cannot open tasks_file named " << argv[1] << " ***\n";
+    return 2;
+  }
+  Utilities::setDelimiter(argv[2][0]);
+  // end of command line processing
 
-    // Acquire the Tasks
-    //
-    std::cout << "Acquiring the Set of Tasks\n--------------------------\n";
-    Task::setDelimiter(Utilities::getDelimiter());
-    std::vector<Task> tasks;
-    while (taskFile) {
-        std::string record;
-        std::getline(taskFile, record);
-        if (taskFile) {
-            try {
-                std::unique_ptr<Task> task(new Task(record));
-                if (!task->getName().empty())
-                    tasks.push_back(std::move(*task));
-            }
-            catch (const std::string& msg) {
-                std::cerr << msg << std::endl;
-            }
-        }
+  // Acquire the Tasks
+  //
+  std::cout << "Acquiring the Set of Tasks\n--------------------------\n";
+  Task::setDelimiter(Utilities::getDelimiter());
+  std::vector<Task> tasks;
+  while (taskFile) {
+    std::string record;
+    std::getline(taskFile, record);
+    if (taskFile) {
+      try {
+        std::unique_ptr<Task> task(new Task(record));
+        if (!task->getName().empty())
+          tasks.push_back(std::move(*task));
+      }
+      catch (const std::string& msg) {
+        std::cerr << msg << std::endl;
+      }
     }
+  }
 
-    // Display the Accepted Set of Tasks
-    //
-    std::cout << "\nAccepted Set of Tasks\n---------------------\n";
-    for (auto& t : tasks)
-        t.display(std::cout);
+  // Display the Accepted Set of Tasks
+  //
+  std::cout << "\nAccepted Set of Tasks\n---------------------\n";
+  for (auto& t : tasks)
+    t.display(std::cout);
 
-    // Check nextTask
-    //
-    std::cout << "\nCheck Next Tasks\n----------------\n";
-    int nerr = 0;
-    for (auto& t : tasks) {
-        try {
-            std::cout << t.getNextTask(Task::passed)->getName() << std::endl;
-            std::cout << t.getNextTask(Task::redirect)->getName() << std::endl;
-        }
-        catch (const std::string& msg) {
-            std::cerr << msg << std::endl;
-            nerr++;
-            if (nerr > 3) {
-                std::cerr << "*** Further checks suspended ***" << std::endl;
-                break;
-            }
-        }
+  // Check nextTask
+  //
+  std::cout << "\nCheck Next Tasks\n----------------\n";
+  int nerr = 0;
+  for (auto& t : tasks) {
+    try {
+      std::cout << t.getNextTask(Task::passed)->getName() << std::endl;
+      std::cout << t.getNextTask(Task::redirect)->getName() << std::endl;
     }
-
-    // Validate the Tasks
-    //
-    std::cout << "\nValidating the Set of Tasks\n---------------------------\n";
-    bool valid = true;
-    for (auto i = 0u; i < tasks.size() && valid; i++) {
-        bool invalid = true;
-        for (auto j = 0u; j < tasks.size() && invalid; j++) {
-            if (tasks[i].validate(tasks[j]))
-                invalid = false;
-        }
-        valid = !invalid;
+    catch (const std::string& msg) {
+      std::cerr << msg << std::endl;
+      nerr++;
+      if (nerr > 3) {
+        std::cerr << "*** Further checks suspended ***" << std::endl;
+        break;
+      }
     }
-    if (!valid)
-        std::cerr << "*** Not all Tasks have been validated ***\n";
-    for (auto& t : tasks)
-        t.display(std::cout);
+  }
 
-    // Terminate
-    //
-    std::cout << "\nDone!\nPress Enter Key to Exit ... ";
-    char c;
-    std::cin.get(c);
+  // Validate the Tasks
+  //
+  std::cout << "\nValidating the Set of Tasks\n---------------------------\n";
+  bool valid = true;
+  for (auto i = 0u; i < tasks.size() && valid; i++) {
+    bool invalid = true;
+    for (auto j = 0u; j < tasks.size() && invalid; j++) {
+      if (tasks[i].validate(tasks[j]))
+        invalid = false;
+    }
+    valid = !invalid;
+  }
+  if (!valid)
+    std::cerr << "*** Not all Tasks have been validated ***\n";
+  for (auto& t : tasks)
+    t.display(std::cout);
+
+  // Terminate
+  //
+  std::cout << "\nDone!\nPress Enter Key to Exit ... ";
+  char c;
+  std::cin.get(c);
 }
