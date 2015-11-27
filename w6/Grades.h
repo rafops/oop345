9dbaff6af37e6c7a9b62dd7adeac846a098c6674
Rafael Moraes <contact@moraesrafael.com>
2015-11-26 20:24:52 -0500

    latest updates

---
+++ w6/Grades.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+class Grades {
+public:
+  Grades(const std::string fileName);
+  // template<typename F>
+  // void displayGrades(std::ostream &os, F letter) const;
+  // template<typename F>
+  // void displayGrades(std::ostream &os, F letter) const;
+  // void displayGrades(std::ostream &os, std::ostream &os, std::function<std::string(double)> letter) const;
+  
+  template<typename F>
+  void displayGrades(std::ostream &os, F letter) const;
+};
