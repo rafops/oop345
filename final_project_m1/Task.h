6d28d32f9f1bac5a7051ef225a9f297fb2af81de
Rafael Moraes <contact@moraesrafael.com>
2015-11-26 21:20:13 -0500

    reviewed Task.h

--- final_project_m1/Task.h
+++ final_project_m1/Task.h
@@ -8,25 +8,25 @@
 #include <string>
 
 class Task {
-    std::string name;
-    std::string slots;
-    std::string nextTask[2];
-    const Task* pNextTask[2];
-    static char delimiter;
-    static size_t field_width;
-  public:
-    enum Quality {
-        passed,
-        redirect
-    };
-    Task(const std::string& record);
-    const std::string& getName() const;
-    unsigned int getSlots() const;
-    bool validate(const Task& task);
-    const Task* getNextTask(Quality quality) const; 
-    void display(std::ostream&) const;
-    static void setDelimiter(const char c);
-    static size_t getFieldWidth();
+  std::string name;
+  std::string slots;
+  std::string nextTask[2];
+  const Task* pNextTask[2];
+  static char delimiter;
+  static size_t field_width;
+public:
+  enum Quality {
+    passed,
+    redirect
+  };
+  Task(const std::string& record);
+  const std::string& getName() const;
+  unsigned int getSlots() const;
+  bool validate(const Task& task);
+  const Task* getNextTask(Quality quality) const; 
+  void display(std::ostream&) const;
+  static void setDelimiter(const char c);
+  static size_t getFieldWidth();
 };
 
 bool operator==(const Task&, const Task&);
