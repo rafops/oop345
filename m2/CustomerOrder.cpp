7a6f4f138280030ba715ed83c8f1f07058204404
Rafael Moraes <contact@moraesrafael.com>
2015-12-07 12:28:14 -0500

    all set except for a bug

--- m2/CustomerOrder.cpp
+++ m2/CustomerOrder.cpp
@@ -3,7 +3,10 @@
 // Rafael Leme de Moraes
 // v1.0 07/12/2015
 
+#include <vector>
+#include <iomanip>
 #include "CustomerOrder.h"
+#include "ItemOrder.h"
 #include "Utilities.h"
 
 char CustomerOrder::delimiter = '|';
@@ -15,57 +18,98 @@ CustomerOrder::CustomerOrder(const std::string& record) {
 
   size_t next_pos = 0;
   bool more = true;
+  std::vector<std::string> tokens;
   Utilities u(field_width);
 
            name    = u.nextToken(record, next_pos, more);
   if(more) product = u.nextToken(record, next_pos, more);
-  // TODO
 
+  /* extract remaining tokens */
+  while(more) tokens.push_back(u.nextToken(record, next_pos, more));
+  
+  /* create ItemOrders for each remaining token */
+  nOrders = tokens.size();
+  order = new ItemOrder[nOrders];
+  for(std::vector<int>::size_type i=0; i<nOrders; i++) {
+    order[i] = ItemOrder(tokens[i]);
+  }
+  
   field_width = u.getFieldWidth();
 }
 
+// Since customer orders are not to be duplicated, 
+// the copy constructor should never be called.
 CustomerOrder::CustomerOrder(const CustomerOrder& customerOrder) {
-  // TODO
+  throw "duplication of customer orders are not allowed!";
 }
 
 CustomerOrder::CustomerOrder(CustomerOrder&& customerOrder) {
-  // TODO
+  if(order) delete[] order;
+  order = nullptr;
+  *this = std::move(customerOrder);
 }
 
 CustomerOrder&& CustomerOrder::operator=(CustomerOrder&& customerOrder) {
-  // TODO
+  if(this!=&customerOrder) {
+    if(order) {
+      delete[] order;
+      order = nullptr;
+      nOrders = 0;
+    }
+    name = customerOrder.name;
+    product = customerOrder.product;
+    while(nOrders < customerOrder.nOrders) {
+      order[nOrders] = customerOrder[nOrders];
+      nOrders++;
+    }
+  }
   return std::move(*this);
 }
 
 CustomerOrder::~CustomerOrder() {
-  // TODO
+  if(order) {
+    delete[] order;
+    order = nullptr;
+    nOrders = 0;
+  }
 }
 
 unsigned int CustomerOrder::noOrders() const {
-  // TODO
-  return 0;
+  return nOrders;
 }
 
 const std::string& CustomerOrder::operator[](unsigned int i) const {
-  // TODO
-  return name; // wrong
+  if(nOrders <= i) throw "index is out of bounds";
+  return order[i].getName();
 }
 
 void CustomerOrder::fill(Item& item) {
-  // TODO
+  for(unsigned int i=0; i<nOrders; i++) {
+    // TODO
+    // if(order[i].getName().compare(item.getName())==0) {
+    //   order[i].fill(item.getCode());
+    //   item++;
+    // }
+  }
 }
 
 void CustomerOrder::remove(Item& item) {
-  // TODO
+  for(unsigned int i=0; i<nOrders; i++) {
+    // TODO
+    // if(order[i].getName().compare(item.getName())==0) order[i].clear();
+  }
 }
 
 bool CustomerOrder::empty() const {
-  // TODO
-  return false;
+  return name.empty();
 }
 
 void CustomerOrder::display(std::ostream& os) const {
-  // TODO
+  os << std::setw(field_width) << name << " : " <<
+        std::setw(field_width) << product << std::endl;
+  for(unsigned int i=0; i<nOrders; i++) {
+    order[i].display(os);
+  }
 }
 
 void CustomerOrder::setDelimiter(const char d) {
