588684a56acdf57dfba220e29d71ed6448bae52e
Rafael Moraes <contact@moraesrafael.com>
2015-12-07 00:49:24 -0500

    item class updated; gitignore updated;

--- m2/Item.cpp
+++ m2/Item.cpp
@@ -3,6 +3,7 @@
 // Rafael Leme de Moraes
 // v1.0 07/12/2015
 
+#include <iomanip>
 #include "Item.h"
 #include "Utilities.h"
 
@@ -10,52 +11,50 @@ char Item::delimiter = '|';
 size_t Item::field_width = 0;
 
 Item::Item(const std::string& record /*= std::string()*/) {
-  details = "no detailed description";
   code = 1;
 
   size_t next_pos = 0;
   bool more = true;
   Utilities u(field_width);
 
-           name               = u.nextToken(record, next_pos, more);
-  if(more) source             = u.nextToken(record, next_pos, more);
-  if(more) destination        = u.nextToken(record, next_pos, more);
-  if(more) details            = u.nextToken(record, next_pos, more);
+           name           = u.nextToken(record, next_pos, more);
+  if(more) source         = u.nextToken(record, next_pos, more);
+  if(more) destination    = u.nextToken(record, next_pos, more);
+  if(more) code = std::stoi(u.nextToken(record, next_pos, more));
+  if(more) details        = u.nextToken(record, next_pos, more);
+
+  if(details.empty()) details = "no detailed description";
 
   field_width = u.getFieldWidth();
 }
 
 bool Item::empty() const {
-  // TODO
-  return false;
+  return name.empty();
 }
 
-void Item::operator++(int i) {
-  // TODO
+void Item::operator++(int) {
+  code++;
 }
 
 unsigned int Item::getCode() const {
-  // TODO
-  return 0;
+  return code;
 }
 
 const std::string& Item::getName() const {
-  // TODO
   return name;
 }
 
 const std::string& Item::getSource() const {
-  // TODO
   return source;  
 }
 
 const std::string& Item::getDestination() const {
-  // TODO
   return destination;
 }
 
 void Item::display(std::ostream& os, bool full /*= false*/) const {
   // TODO
+  os << std::endl;
 }
 
 void Item::setDelimiter(const char d) {
